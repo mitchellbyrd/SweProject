a0e4aba48aa833ddda7e086d6db64533a3d4b516
Marcus Smith <marcusmax94@yahoo.com>
2016-02-04 19:23:16 -0500

    test for the calendar fragment branch

--- app/src/main/java/com/sweproject/calorietracker/MainActivity.java
+++ app/src/main/java/com/sweproject/calorietracker/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity{
 
 		sTabLayout = (TabLayout) findViewById(R.id.activity_tabs);
 		sTabLayout.setupWithViewPager(sViewPager);
+		// Comment of calendar fragment branch
 	}
 
 	@Override
