48b9cdd6334d6259c3d02c6e722762e98e1844fa
Marcus Smith <marcusmax94@yahoo.com>
2016-02-15 12:35:52 -0500

    For danny's login I created a button event for his submit button to send the user to the calendar. Did error checking for the bundle in the login For registration added a event for the button. As of now, the app will flow from login to registration to calendar

--- app/src/main/java/com/sweproject/calorietracker/LoginFragment.java
+++ app/src/main/java/com/sweproject/calorietracker/LoginFragment.java
@@ -1,14 +1,54 @@
 package com.sweproject.calorietracker;
 
-import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by danny.oneal on 2/10/2016.
  */
-public class LoginFragment extends Fragment{
+public class LoginFragment extends Fragment implements View.OnClickListener {
 
-    public void onSubmit(View view)
-    {
-    }
+	private String userName;
+
+	@Override
+	public void onActivityCreated(Bundle savedInstance) {
+		super.onActivityCreated(savedInstance);
+		Bundle lilBundle = getArguments();
+		userName = lilBundle != null ? lilBundle.getString("userName") : "";
+		boolean isNewUser = isNewUser(userName);
+
+		Button submitBtn = (Button) getActivity().findViewById(R.id.fragment_login_submitBtn);
+		submitBtn.setOnClickListener(this);
+
+		if (isNewUser) {
+			RegistrationFragment regFragment = new RegistrationFragment();
+			MainActivity.nextFragment(this, regFragment, null, false, false);
+		} else {
+			FragmentCalendar calendarFragment = new FragmentCalendar();
+			MainActivity.nextFragment(this, calendarFragment, null, false, false);
+		}
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View root = inflater.inflate(R.layout.fragment_login, container, false);
+		return root;
+	}
+
+	public boolean isNewUser(String userName) {
+	    return true;
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+			case R.id.fragment_login_submitBtn:
+				MainActivity.nextFragment(this, new FragmentCalendar(), null, true, false);
+				break;
+		}
+	}
 }
