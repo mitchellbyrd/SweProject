e3f380a6486a743343eae5accd8f6c26ac4d80da
Marcus Smith <marcusmax94@yahoo.com>
2016-02-17 17:57:33 -0500

    Loads amazon.com into a webview to show we can redirect to a webpage.

--- app/src/main/java/com/sweproject/calorietracker/MainActivity.java
+++ app/src/main/java/com/sweproject/calorietracker/MainActivity.java
@@ -1,22 +1,14 @@
 package com.sweproject.calorietracker;
 
-import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity{
-
-	private static FragmentManager sFragmentManager;
-	protected SQLiteDatabase sqLiteDatabase;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,22 +18,12 @@ public class MainActivity extends AppCompatActivity{
 		Toolbar toolbar = (Toolbar) findViewById(R.id.activity_toolbar);
 		setSupportActionBar(toolbar);
 
-		sFragmentManager = getSupportFragmentManager();
+		WebView myWebView = (WebView) findViewById(R.id.activity_webView);
 
-		Window window = getWindow();
-
-		// On devices with Android Lolipop, change the color of the status bar to the colorPrimaryDark
-		if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-			window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-			window.setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
-		}
-
-		// If first time opening app
 		if (savedInstanceState == null) {
-			nextFragment(null, new LoginFragment(), null, false, false);
+			myWebView.loadUrl("http://www.amazon.com");
+			myWebView.setWebViewClient(new WebViewClient());
 		}
-		// else will show the last visible fragment (Android destroys activity during rotation)
 	}
 
 	@Override
@@ -64,44 +46,5 @@ public class MainActivity extends AppCompatActivity{
 		}
 
 		return super.onOptionsItemSelected(item);
-	}
-
-
-	/** Commits replacement transactions with fragments.
-	 *
-	 * @param fromFrag Fragment that should be replaced. Pass null transition should be sliding left to right instead of up and down
-	 * @param toFrag Instance of the fragment that should be called next.
-	 * @param bun Data that should be set to the fragment or null if none.
-	 * @param add True if fragment should be added to the backstack.
-	 * @param clear True if system should clear backstack then add fragment
-	 */
-	public static void nextFragment(Fragment fromFrag, Fragment toFrag, Bundle bun, boolean add, boolean clear){
-
-		if (bun != null){
-			toFrag.setArguments(bun);
-		}
-		if (clear){
-			// Clears the back stack, leaving the first fragment added to be displayed
-			sFragmentManager.popBackStack(sFragmentManager.getBackStackEntryAt(0).getId(), 0);
-		}
-		if (add){
-			sFragmentManager.beginTransaction()
-					.replace(R.id.activity_container, toFrag)
-					.addToBackStack(null)
-					.commit();
-		} else {
-			sFragmentManager.beginTransaction()
-					.replace(R.id.activity_container, toFrag)
-					.commit();
-		}
-	}
-
-	@Override
-	public void onBackPressed() {
-		if (getSupportFragmentManager().getBackStackEntryCount() != 0) {
-			getSupportFragmentManager().popBackStack();
-		} else {
-			super.onBackPressed();
-		}
 	}
 }
