dc599a9e3f2dcc2f31ded6896e4f9916949d168e
Marcus Smith <marcusmax94@yahoo.com>
2016-03-15 18:41:23 -0400

    Updated the Users class fields

--- app/src/main/java/com/sweproject/calorietracker/Users.java
+++ app/src/main/java/com/sweproject/calorietracker/Users.java
@@ -6,4 +6,13 @@ package com.sweproject.calorietracker;
 public class Users {
 	public String Id;
 	public String NameFirst;
+	public String NameLast = "Default";
+	public String EmailAddress = "Default";
+	public String Password = "Default";
+	public String DateBirth = "Default";
+	public int Weight = 100;
+	public int PreferedCalorieLimit = 100;
+	public int PreferedFatLimit = 100;
+	public int PreferedCarbLimit = 100;
+	public int PreferedProteinLimit = 100;
 }
