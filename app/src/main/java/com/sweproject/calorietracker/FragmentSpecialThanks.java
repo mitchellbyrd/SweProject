8c075ef40cd2e7541f7b234165532e0181f6858a
Marcus Smith <marcusmax94@yahoo.com>
2016-04-27 11:26:28 -0400

    Add page to give credit

--- app/src/main/java/com/sweproject/calorietracker/FragmentSpecialThanks.java
+++ app/src/main/java/com/sweproject/calorietracker/FragmentSpecialThanks.java
@@ -18,6 +18,6 @@ public class FragmentSpecialThanks extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_report, container, false);
+		return inflater.inflate(R.layout.fragment_special_thanks, container, false);
 	}
 }
